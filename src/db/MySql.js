2c9b121c49c37a8ce530ce4c264cd069236d34b8
kush <kushbhardwaj8800@gmail.com>
2024-08-24 05:56:38 +0530

    1st table create

--- src/db/MySql.js
+++ src/db/MySql.js
@@ -11,12 +11,12 @@ connectConection.connect((err)=>{
         console.log('failed to connect Mysql');
         return;
     }
-    // var sql = 'create table customer (name varchar(25) ,age int)';
-    // connectConection.query(sql, function(err,result){
-    //     if(err)
-    //         throw err;
+    var sql = 'create table customer (name varchar(25) ,age int)';
+    connectConection.query(sql, function(err,result){
+        if(err)
+            throw err;
         console.log("table created")
-    // })
+    })
     console.log('MySql Connected')
 })
 module.exports = connectConection;
